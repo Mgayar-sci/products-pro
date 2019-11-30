c7f088f695bb0f276ceb95666e22d3cac606306c
Mgayar-Sci <dr.mohamedelgayar@gmail.com>
2019-11-30 20:02:22 +0200

    Added details to product

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Grid,
-  CardActionArea,
   Card,
   CardContent,
   Typography,
   Hidden,
-  CardMedia
+  CardMedia,
+  Button
 } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
@@ -27,9 +27,13 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Blog(props) {
+export default function Product(props) {
   const classes = useStyles();
   const product = props.product || {};
+  const showMore = product.description
+    ? product.description.length > 100
+    : false;
+  const description = showMore? product.description.slice(0,87) : product.description;
   
   const addToCart = () => {
     props.addToCart(1, product.id);
@@ -40,54 +44,57 @@ export default function Blog(props) {
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea>
-        <Card className={classes.card}>
-          <Hidden smUp>
-            <CardMedia
-              className={classes.cardMediaUp}
-              image="/Halloween_Sale.PNG"
-              // image="https://source.unsplash.com/random"
-              title="Image title"
-            />
-            <br/>
-          </Hidden>
-          </Card>
-        <Card className={classes.card}>
-          <div className={classes.cardDetails}>
-            <CardContent>
-              <Typography component="h2" variant="h5">
-                {product.title}
-              </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                {product.price}
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {product.description + " "}
-                <Typography variant="subtitle2" color="primary" component="a" href={`/product/${product.id}`}>
+      <Card className={classes.card}>
+        <Hidden smUp>
+          <CardMedia
+            className={classes.cardMediaUp}
+            image="/Halloween_Sale.PNG"
+            // image="https://source.unsplash.com/random"
+            title={product.title}
+          />
+          <br />
+        </Hidden>
+      </Card>
+      <Card className={classes.card}>
+        <div className={classes.cardDetails}>
+          <CardContent>
+            <Typography component="h2" variant="h5">
+              {product.title}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              {product.price}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {description + " "}
+              {showMore && (
+                <Typography
+                  variant="subtitle2"
+                  component="a"
+                  href={`/product/${product.id}`}
+                >
                   More details
                 </Typography>
-              </Typography>
-              <Typography variant="button" onClick={addToCart}>
-                Add to cart
-              </Typography>
-              <Typography variant="button" onClick={subToCart}>
+              )}
+            </Typography>
+            <Button color="primary" onClick={addToCart}>
+              Add to cart
+            </Button>
+            {props.isInCart && (
+              <Button color="secondary" onClick={subToCart}>
                 Remove from cart
-              </Typography>
-              
-              {/* <button onClick={addToCart}>+</button>
-                <button onClick={subToCart}>-</button> */}
-            </CardContent>
-          </div>
-          <Hidden xsDown>
-            <CardMedia
-              className={classes.cardMedia}
-              image="/Halloween_Sale.PNG"
-              // image="https://source.unsplash.com/random"
-              title="Image title"
-            />
-          </Hidden>
-        </Card>
-      </CardActionArea>
+              </Button>
+            )}
+          </CardContent>
+        </div>
+        <Hidden xsDown>
+          <CardMedia
+            className={classes.cardMedia}
+            image="/Halloween_Sale.PNG"
+            // image="https://source.unsplash.com/random"
+            title="Image title"
+          />
+        </Hidden>
+      </Card>
     </Grid>
   );
 }
